2da20a3161462b35d16d806a266c8403f1a379c9
ranranSama <1191896830.com>
2017-10-04 20:57:37 +0800

    hello ranranSama

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
 int main(int argc, char** argv) {
+	
+	
+	
+	
 	std::cout << "hello github" <<std::endl;
+	std::cout << "hello ranranSama" <<std::endl;
+
+
 	return 0;
 }
