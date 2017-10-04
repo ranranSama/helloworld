70cd14433d568ac709e345395b9fd9c31f964f33
ranranSama <1191896830.com>
2017-10-04 20:54:54 +0800

    hello

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	std::cout << "hello github" <<std::endl;
+	return 0;
+}
